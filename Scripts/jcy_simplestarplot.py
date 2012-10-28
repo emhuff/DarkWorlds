bbc9d5fbc67ab8a3ae1b4e3b00a152ff54f905d6
Julia Young <jyoung@cosmos7.mps.ohio-state.edu>
2012-10-28 16:47:27 -0400

    Added file

---
+++ Scripts/jcy_simplestarplot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# ====================================================================
+# Authors:
+# Maria Elidaiana - mariaeli@cbpf.br
+# Mandeep S. Gill - 
+# Current July 7, 2011
+# modified by JCY
+# ====================================================================
+
+"""Code to plot ellips in the standard WL conventions"""
+# Usage example : python simplestarplot.py "a611.starsOnStars.test.lhsChip.cat"
+import sys
+import pylab as pl
+import numpy as np
+
+
+def plotstars(x,y,e0_bef,e1_bef):
+  scalefac=3000.0
+  emag=np.sqrt(e0_bef**2+e1_bef**2)
+  alpha=np.arctan2(e1_bef,e0_bef)/2.0
+  delta_x=np.cos(alpha)*emag*scalefac
+  delta_y=np.sin(alpha)*emag*scalefac
+  for i in range(len(x)):
+    pl.plot([x[i]-delta_x[i]/2,x[i]+delta_x[i]/2],[y[i]-delta_y[i]/2, y[i]+delta_y[i]/2], 'k')
+
+def plotstars_s(x,y,e0_bef,e1_bef, scalefac):
+  emag=np.sqrt(e0_bef**2+e1_bef**2)
+  alpha=np.arctan2(e1_bef,e0_bef)/2.0
+  delta_x=np.cos(alpha)*emag*scalefac
+  delta_y=np.sin(alpha)*emag*scalefac
+  for i in range(len(x)):
+    pl.plot([x[i]-delta_x[i]/2,x[i]+delta_x[i]/2],[y[i]-delta_y[i]/2, y[i]+delta_y[i]/2], 'k')
+
+def plotstars_color(x,y,e0_bef,e1_bef,color):
+  scalefac=5000.0
+  emag=np.sqrt(e0_bef**2+e1_bef**2)
+  alpha=np.arctan2(e1_bef,e0_bef)/2.0
+  delta_x=np.cos(alpha)*emag*scalefac
+  delta_y=np.sin(alpha)*emag*scalefac
+  for i in range(len(x)):
+    pl.plot([x[i]-delta_x[i]/2,x[i]+delta_x[i]/2],[y[i]-delta_y[i]/2, y[i]+delta_y[i]/2], color)
+
